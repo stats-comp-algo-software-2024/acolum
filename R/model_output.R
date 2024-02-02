d0fbc52eb28e5b36510166382b36fa05f3855628
Alyssa Columbus <21285656+acolum@users.noreply.github.com>
2024-02-01 21:59:10 -0500

    Add and export S3 methods coef, vcov, and print

---
+++ R/model_output.R
@@ -0,0 +1,23 @@
+#' @export
+coef.hglm <- function(hglm_out){
+  # Warn that the S3 method is yet to be implemented
+  warning('This method is yet to be implemented.')
+  # TODO: Implement coef.hglm S3 method
+  cat('The coefficients are ...')
+}
+
+#' @export
+vcov.hglm <- function(hglm_out){
+  # Warn that the S3 method is yet to be implemented
+  warning('This method is yet to be implemented.')
+  # TODO: Implement vcov.hglm S3 method
+  cat('The variance-covariance matrix is ...' )
+}
+
+#' @export
+print.hglm <- function(hglm_out){
+  # Warn that the S3 method is yet to be implemented
+  warning('This method is yet to be implemented.')
+  # TODO: Implement print.hglm S3 method
+  cat('The model is ...')
+}
