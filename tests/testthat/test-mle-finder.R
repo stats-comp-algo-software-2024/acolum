3dfa94853a9c7d9177f331fa16e33216d9fc5614
Alyssa Columbus <21285656+acolum@users.noreply.github.com>
2024-02-16 20:44:47 -0500

    Copy from the instructor's repo the (currently failing) test of the Newton's method output against BFGS in `test-mle-finder.R`

--- tests/testthat/test-mle-finder.R
+++ tests/testthat/test-mle-finder.R
@@ -47,3 +47,16 @@ test_that('MLEs estimated via pseudo-inverse and via BFGS match (should return `
                            model = 'linear', method = 'BFGS')
   expect_true(are_all_close(v = coef(pseudoinv_result), w = coef(BFGS_result)))
 })
+
+test_that('MLEs estimated via Newtons method and via BFGS coincide on logit model (should return `TRUE`)', {
+  n_obs <- 32; n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
+  design <- data$design; outcome <- data$outcome
+  via_newton_out <- hiper_glm(design, outcome, model = 'logit')
+  via_bfgs_out <- hiper_glm(
+    design, outcome, model = 'logit', option = list(mle_solver = 'BFGS')
+  )
+  expect_true(are_all_close(
+    coef(via_newton_out), coef(via_bfgs_out), abs_tol = 1e-2, rel_tol = 1e-2
+  ))
+})
